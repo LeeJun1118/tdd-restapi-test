b1468afe72cfeccc2e2bc82cf0a372280584c3ae
LeeJun <ljun1118@ks.ac.kr>
2019-08-11 17:29:47 +0900

    Add 프로젝트 초기 생성 - 스프링 부트를 사용해서 REST-API를 활용할 수 있도록 구성하였음

---
+++ src/main/java/com/leejun/restapitestalone/RestApiTestAloneApplication.java
@@ -0,0 +1,13 @@
+package com.leejun.restapitestalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiTestAloneApplication.class, args);
+    }
+
+}
